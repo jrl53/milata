2ec8e778d678989a2be332ec7725fd7d044e9b05
jrl53 <josericardoleonlee@gmail.com>
2014-12-28 17:35:37 -0600

    ;lajfds

--- server/cleanJob.js
+++
@@ -1,7 +0,0 @@
-var Firebase = require("firebase");
-
-var fbLiveLocs = new Firebase("https://boiling-inferno-6943.firebaseio.com/liveLocs");
-
-fbLiveLocs.on("child_added", function(){
-    console.log("added!");
-});
